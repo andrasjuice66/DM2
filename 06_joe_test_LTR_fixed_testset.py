9dc1008148764f6d37b1f2b899dda60db6c4f588
joesidley <joesidley1@gmail.com>
2024-05-12 11:44:44 +0200

    add ltr xgboost

--- 06_joe_test_LTR_fixed_testset.py
+++ 06_joe_test_LTR_fixed_testset.py
@@ -8,11 +8,11 @@ def load_data(filename):
 
 # Calculate the relevance scores for training data
 def calculate_relevance(df):
-    book_weight = 10
-    click_weight = 5
+    book_weight = 5
+    click_weight = 2
     total_positions = df['position'].max()
     # Ensure the relevance score is an integer
-    df['relevance_score'] = np.round((df['booking_bool'] * book_weight) + (df['click_bool'] * click_weight) - (df['position'] / total_positions * 4)).astype(int)
+    df['relevance_score'] = np.round((df['booking_bool'] * book_weight) + (df['click_bool'] * click_weight) +total_positions/df['position']/4).astype(int)
     return df
 
 
@@ -35,12 +35,54 @@ def train_model(dtrain, params):
 def predict(model, dtest):
     predictions = model.predict(dtest)
     return predictions
+def get_time_of_day(hour):
+    if 5 <= hour < 12:
+        return 1
+    elif 12 <= hour < 17:
+        return 2
+    elif 17 <= hour < 21:
+        return 3
+    else:
+        return 4
 
 # Main function to run the program
 def main():
     train_df = load_data('dmt-2024-2nd-assignment/training_set_VU_DM.csv')
+
+    train_df['date_time'] = pd.to_datetime(train_df['date_time'])
+    train_df['time_of_day'] = train_df['date_time'].dt.hour.apply(get_time_of_day)
+    train_df['is_weekend'] = train_df['date_time'].dt.dayofweek >= 5
+    train_df['price_per_night'] = train_df['price_usd'] / train_df['srch_length_of_stay']
+    train_df['star_rating_difference'] = train_df['prop_starrating'] - train_df['visitor_hist_starrating'].fillna(train_df['prop_starrating'])
+    train_df['is_domestic'] = (train_df['prop_country_id'] == train_df['visitor_location_country_id']).astype(int)
+    train_df[[f'comp{i}_rate' for i in range(1, 9)]].fillna(0, inplace=True)
+
+    train_df['rate_advantage_count'] = (train_df[[f'comp{i}_rate' for i in range(1, 3)]].fillna(0) > 0).sum(axis=1)
+    train_df['rate_disadvantage_count'] = (train_df[[f'comp{i}_rate' for i in range(1, 3)]].fillna(0) < 0).sum(axis=1)
+    train_df['availability_advantage_count'] = (train_df[[f'comp{i}_inv' for i in range(1, 3)]].fillna(0) > 0).sum(axis=1)
+    train_df['avg_rate_percent_diff'] = train_df[[f'comp{i}_rate_percent_diff' for i in range(1, 3)]].mean(axis=1, skipna=True)
+    train_df['max_rate_percent_diff'] = train_df[[f'comp{i}_rate_percent_diff' for i in range(1, 3)]].max(axis=1, skipna=True)
+    train_df['min_rate_percent_diff'] = train_df[[f'comp{i}_rate_percent_diff' for i in range(1, 3)]].min(axis=1, skipna=True)
+
     test_df = load_data('dmt-2024-2nd-assignment/test_set_VU_DM.csv')
-    print("both loaded")
+
+
+    test_df['date_time'] = pd.to_datetime(test_df['date_time'])
+    test_df['time_of_day'] = test_df['date_time'].dt.hour.apply(get_time_of_day)
+    test_df['is_weekend'] = test_df['date_time'].dt.dayofweek >= 5
+    test_df['price_per_night'] = test_df['price_usd'] / test_df['srch_length_of_stay']
+    test_df['star_rating_difference'] = test_df['prop_starrating'] - test_df['visitor_hist_starrating'].fillna(test_df['prop_starrating'])
+    test_df['is_domestic'] = (test_df['prop_country_id'] == test_df['visitor_location_country_id']).astype(int)
+    test_df[[f'comp{i}_rate' for i in range(1, 9)]].fillna(0, inplace=True)
+
+    test_df['rate_advantage_count'] = (test_df[[f'comp{i}_rate' for i in range(1, 3)]].fillna(0) > 0).sum(axis=1)
+    test_df['rate_disadvantage_count'] = (test_df[[f'comp{i}_rate' for i in range(1, 3)]].fillna(0) < 0).sum(axis=1)
+    test_df['availability_advantage_count'] = (test_df[[f'comp{i}_inv' for i in range(1, 3)]].fillna(0) > 0).sum(axis=1)
+    test_df['avg_rate_percent_diff'] = test_df[[f'comp{i}_rate_percent_diff' for i in range(1, 3)]].mean(axis=1, skipna=True)
+    test_df['max_rate_percent_diff'] = test_df[[f'comp{i}_rate_percent_diff' for i in range(1, 3)]].max(axis=1, skipna=True)
+    test_df['min_rate_percent_diff'] = test_df[[f'comp{i}_rate_percent_diff' for i in range(1, 3)]].min(axis=1, skipna=True)
+    
+    
     
     train_df = calculate_relevance(train_df)
     
